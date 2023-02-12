8f09751b659ca8a681e52e1b7008261918e1571c
David Luis Wiegandt <wigandtd@hu-berlin.de>
2023-02-12 11:13:13 +0100

    minor css cleanup

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const tagName = (name) => `cv-${name}`;
+
+function register(Constructor) {
+    if ("templateId" in Constructor && typeof Constructor.templateId === "string") {
+        customElements.define(Constructor.templateId, Constructor);
+    }
+}
+
 class TagElement extends HTMLElement {
-    static #template = document.getElementById("cv-tag").content;
+    static #templateId = tagName("tag");
+    static #template = document.getElementById(this.#templateId).content;
 
     #shadowRoot;
 
@@ -9,6 +18,29 @@ class TagElement extends HTMLElement {
         this.#shadowRoot = this.attachShadow({ mode: "closed" });
         this.#shadowRoot.appendChild(TagElement.#template.cloneNode(true));
     }
+
+    static get templateId() {
+        return TagElement.#templateId;
+    }
 }
 
-customElements.define("cv-tag", TagElement);
+class TagListElement extends HTMLElement {
+    static #templateId = tagName("tag-list");
+    static #template = document.getElementById(this.#templateId).content;
+
+    #shadowRoot;
+
+    constructor() {
+        super();
+
+        this.#shadowRoot = this.attachShadow({ mode: "closed" });
+        this.#shadowRoot.appendChild(TagListElement.#template.cloneNode(true));
+    }
+
+    static get templateId() {
+        return this.#templateId;
+    }
+}
+
+register(TagElement);
+register(TagListElement);
